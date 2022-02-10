dbaa9e9280ab05e1642274bb7dda437987f4da51
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-02-10 01:08:49 -0300

    Deep copy con recursividad

--- js/deepCopyConEecursividad.js
+++ js/deepCopyConEecursividad.js
@@ -1,23 +1,34 @@
-const isObject = subject => (typeof subject) === 'object'
+function isObjectOrArray(element) {
+    let elementIs = 'nothing'
+    const typeElement = Object.prototype.toString.call(element)
 
-const isArray = subject => Array.isArray(subject)
+    switch (typeElement) {
+        case '[object Object]':
+            elementIs = 'object'
+            break
+        case '[object Array]':
+            elementIs = 'array'
+            break
+    }
+
+    return elementIs;
+}
 
 function deepCopy (subject) {
-    debugger
+    // debugger
     let copySubject
-    const subjectIsArray = isArray(subject)
-    const subjectIsObject = isObject(subject)
+    const subjectIs = isObjectOrArray(subject)
 
-    if (subjectIsArray) copySubject = []
-    if (subjectIsObject && !subjectIsArray) copySubject = {}
-    if (!subjectIsObject && !subjectIsArray) return subject
+    if (subjectIs === 'array') copySubject = []
+    if (subjectIs === 'object') copySubject = {}
+    if (!(subjectIs === 'array') && !(subjectIs === 'object')) return subject
 
     for (const key in subject) {
-        const keyIsObject = isObject(subject[key])
+        const keyIsObject = isObjectOrArray(subject[key])
 
-        if (subjectIsArray) copySubject.push(subject[key])
-        if (keyIsObject && !subjectIsArray) copySubject[key] = deepCopy(subject[key])
-        if (!keyIsObject && !subjectIsArray) copySubject[key] = subject[key]
+        if (subjectIs === 'array') copySubject.push(subject[key])
+        if (keyIsObject === 'object') copySubject[key] = deepCopy(subject[key])
+        if (!(subjectIs === 'array') && !(keyIsObject === 'object')) copySubject[key] = subject[key]
     }
 
     return copySubject
